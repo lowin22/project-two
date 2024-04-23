68259ec67ab1fed2357d740fde0d7f98488dc5d6
lowin22 <83418156+lowin22@users.noreply.github.com>
2024-04-22 18:55:48 -0600

    cambios
    necesarios

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+
+  return (
+    <>
+     <div className="h-screen flex justify-center items-center">
+
+hola mundo
+<h1 className="text-3xl font-bold underline">
+hello world!
+
+</h1>
+</div>
+
+    </>
+  )
+}
+
+export default App
